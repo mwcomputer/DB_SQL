e550cad33b60525123c322398c8ca6f67e2a8b1a
mwcomputer <kolbenfresser5@googlemail.com>
2021-07-06 07:02:13 +0000

    Neue Befehle

--- 02_SQL_Tables/02_db_table_SQL.sql
+++ 02_SQL_Tables/02_db_table_SQL.sql
@@ -6,3 +6,20 @@ SHOW DATABASES;
 /* DB boo löschen fals vorhanden*/
 DROP DATABASE IF EXISTS boo;
 
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* DB auswählen */
+USE boo;
+
+/* Tabelle anlegen, falls noch nicht vorhanden */
+CREATE TABLE IF NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
+);
+
+SHOW TABLES;
+
+SHOW DATABASES;
+
