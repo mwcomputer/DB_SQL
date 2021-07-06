312510edd267f11c7e9ff6ba5b26d2d61465e555
mwcomputer <kolbenfresser5@googlemail.com>
2021-07-06 09:10:22 +0000

    Erstellung einer Neuen Datei 02b_db_table_SQL.sql

---
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -0,0 +1,55 @@
+/* -------- Strukturen ---------*/
+
+
+
+/* DB boo löschen fals vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* DB auswählen */
+USE boo;
+
+
+/*Tabelle Test löschen, fals vorhanden */
+DROP TABLE IF EXISTS boo.test;
+
+/* 
+
+NULL wird nicht mehr zugelassen
+DEFAULT-Werte eintragen
+
+
+*/
+
+
+
+/* Tabelle anlegen, falls noch nicht vorhanden */
+CREATE TABLE IF NOT EXISTS boo.test
+(
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
+    age INT NOT NULL DEFAULT 0
+);
+
+
+/* Strukturen der Tabelle anzeigen */
+
+DESCRIBE test;
+
+/* ------------ Daten------------------*/
+
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
+# Doppelte Datensätze werden zugelassen !
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+/*----Tabelleninhalte anzeigen*/
+SELECT * FROM test;
+
+
+
+
+
+
+
