5eb97e48375f22ec02748ae4cb2bf54717fffbe7
mwcomputer <kolbenfresser5@googlemail.com>
2021-07-08 10:10:22 +0000

    Auflistung Nummerisch

--- 05_SQL_SELECT/05_3_que_sort.sql
+++ 05_SQL_SELECT/05_3_que_sort.sql
@@ -5,20 +5,36 @@ USE boo;
 
 
 # --- ASC / DSC 
-# --- Sortieren nach Sektor / Aufsteigend  (alphabetisch)
+# --- Sortieren nach Sektor / Aufsteigend  (Alphabetisch)
 
 
 
-SELECT 
-    ticker AS SYM,
-    c_name AS "Unternehmen",
-    CONCAT(sector, " | ", industry) AS "Operations"
+#SELECT 
+ #   ticker AS SYM,
+  #  c_name AS "Unternehmen",
+   # CONCAT(sector, " | ", industry) AS "Operations"
+#FROM ccc_list
+#ORDER BY ticker ASC 
+#ORDER BY ticker DESC  
+#ORDER BY sector,industry ASC
+#ORDER BY sector ASC,industry DESC 
+
+#LIMIT 20;
+
+# ---Alphabetisch Sortieren nach Sektor / Aufsteigend (nummerisch) 
+
+
+SELECT
+    ticker AS "SYMBOL",
+    c_name "Unternehmen",
+    CONCAT(sector, " | ", industry) "Operatiobs",
+    price "Kurs in $",
+    payouts "Zahlungen p.a",
+    no_yrs "Jahre ++"
+    
 FROM ccc_list
-LIMIT 200,5;
-
-
-
-
+ORDER BY price ASC #beginnent mit günstigster Aktie. 
+LIMIT 20;
 
 
 
