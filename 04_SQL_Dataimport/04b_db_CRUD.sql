d661886711bbe3706a7305e0e7eb5cb5daf4f942
mwcomputer <kolbenfresser5@googlemail.com>
2021-07-08 07:07:54 +0000

    Rename

--- 04_SQL_Dataimport/04b_db_CRUD.sql
+++ 04_SQL_Dataimport/04b_db_CRUD.sql
@@ -2,3 +2,18 @@
 # Create --> siehe 04a ....
 
 
+# Update ---> Änerung in bestehender Tabelle
+
+#UPDATE cats SET cat_name = "Alonzo"; # Achtung gesamte Spalte wird überschrieben!
+ #UPDATE cats SET cat_name = "Alonzo" WHERE id = 2;
+
+#SELECT * From cats;
+
+#UPDATE cats SET cat_name = "Big Alonzo" WHERE fur_color = "grey";
+/* READ - SELECT : Ergebnistabelle */
+
+SELECT * From cats; # gesamte Tabelle
+SELECT cat_name From cats;
+SELECT cat_name,cat_age From cats;
+SELECT cat_age, cat_name From cats;
+SELECT cat_name AS "Name" , cat_age AS "Alter" FROM cats;
