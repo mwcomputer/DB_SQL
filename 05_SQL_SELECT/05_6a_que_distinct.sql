64e8d8ac4c3a7b2e5de7bdf61d471ccf8ce394f9
mwcomputer <kolbenfresser5@googlemail.com>
2021-07-09 08:56:12 +0000

    Neue Funktion

--- 05_SQL_SELECT/05_6a_que_distinct.sql
+++ 05_SQL_SELECT/05_6a_que_distinct.sql
@@ -1,8 +1,19 @@
 /* QUERIES - DISTINCT*/ 
 
 -- alle Sektoren 
-SELECT
+/*SELECT
     sector
 FROM ccc_list
 ORDER BY sector ASC
-LIMIT 20;
+LIMIT 20;*/
+
+
+-- nur das erste Auftreten von Sektoren 
+-- welche Verschiedenen Sektoren gibt es 
+
+SELECT
+    DISTINCT sector Industriesektoren
+FROM ccc_list
+ORDER BY sector ASC
+#LIMIT 20
+;
