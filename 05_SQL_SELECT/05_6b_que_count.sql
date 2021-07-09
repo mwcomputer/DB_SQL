a3977f93da9f88f5839ef2889e17b9e8a33e55cf
mwcomputer <kolbenfresser5@googlemail.com>
2021-07-09 09:44:18 +0000

    sektoren

--- 05_SQL_SELECT/05_6b_que_count.sql
+++ 05_SQL_SELECT/05_6b_que_count.sql
@@ -8,3 +8,10 @@ https://dev.mysql.com/doc/refman/5.6/en/group-by-handling.html
 https://dev.mysql.com/doc/refman/5.6/en/aggregate-functions.html#function_count-distinct
 */
 
+
+
+SELECT
+DISTINCT sector Industriesektoren
+FROM ccc_list
+ORDER BY sector ASC;
+
